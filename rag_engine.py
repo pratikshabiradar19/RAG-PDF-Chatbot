c7688cdf4ef55299f9d438049b7d26e2c244591c
Pratiksha Biradar <biradarpratiksha296@gmail.com>
2026-04-25 18:44:36 +0530

    Fix langchain imports for deployment

--- rag_engine.py
+++ rag_engine.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
